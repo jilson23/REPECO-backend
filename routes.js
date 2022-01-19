e8dd2eb404991dfd8b2bbd2bc7a22a96ba95dda7
Walther <walther.vergaray@gmail.com>
2022-01-19 10:57:21 -0500

    fixing errors payment

--- routes.js
+++ routes.js
@@ -5,7 +5,6 @@ const reserve = require('./api/reserve')
 const authLocal = require('./auth/local');
 
 const invoices = require('./api/invoice');
-const payment = require('./api/payment');
 
 // defining routes
 function routes(app) {
@@ -14,7 +13,7 @@ function routes(app) {
   app.use('/api/hotels', hotel);
   app.use('/api/reserves', reserve);
   app.use('/api/invoices', invoices);
-  app.use('/api/payments', payment);
+
   // file route
 
   // auth routes
