b65fbfe8ed03fa7c15038bdff9f0a413e3dda350
jilson jimenez <jilsonjimenez@gmail.com>
2022-01-28 11:12:20 -0500

    actualization endpoint post rooms

--- api/room/room.controller.js
+++ api/room/room.controller.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const cloudinary = require('cloudinary').v2;
+
 const {
   createRoom,
   deleteRoom,
@@ -12,7 +15,6 @@ const { findOneHotel } = require('../hotel/hotel.services')
 async function getAllRoomsHandler(req, res) {
   try {
     const rooms = await getAllRooms();
-    console.log('rooms', rooms)
     return res.status(200).json(rooms)
   } catch (error) {
     return res.status(500).json({ error: error.message })
@@ -60,8 +62,42 @@ async function getRoomsbyHotelHandler(req, res) {
 }
 
 async function createRoomHandler(req, res) {
+  const { files, body: { file }, user: { _id } } = req;
+
+  const results = [];
+  for (const file of files) {
+    try {
+      const result = await cloudinary.uploader.upload(file.path, { folder: 'rooms', filename_override: file.originalname });
+      results.push({ imageName: result.original_filename, serviceUrl: result.public_id });
+    } catch (error) {
+      return res.status(500).json(error);
+    } finally {
+      fs.unlinkSync(file.path);
+    }
+  }
+
   try {
-    const Room = await createRoom(req.body);
+    const hotel = await findOneHotel(_id);
+
+    const servicedata = JSON.parse(file[4]);
+
+    if (!hotel) {
+      return res.status(404).json({
+        message: 'Hotel not found'
+      });
+    }
+
+    const payload = {
+      title: file[0],
+      description: file[1],
+      capacity: file[2],
+      price: file[3],
+      services: servicedata,
+      images: results,
+      hotel: hotel._id,
+    }
+
+    const Room = await createRoom(payload);
     return res.status(201).json(Room);
   } catch (error) {
     return res.status(500).json({ error: error.message });
